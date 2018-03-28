3d6a7c1ff64b90e5b13933f946fee1b0b5db300a
Sagar Vakkala <sagarwakawaka@gmail.com>
2018-03-28 20:37:19 +0530

    qrcode-terminal implementation
    A qr-code of the LocalIP is being shown on the bash now. As of now, It's generating the barcode of a predefined localhost ip

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ var express = require('express');
 
 var app = express();
 
+var qrTerminal = require('qrcode-terminal');
+
 const port = 3000 || process.env.PORT;
 
 app.listen(port, (req,res) => {
   console.log("Server is live");
+  qrTerminal.generate("http://192.168.1.2:3000");
 });
 
 app.get('/', (req,res) => {
   console.log("A GET req to the server was recieved");
-  res.sendFile(__dirname + "/bitcoin.pdf");
+  res.sendFile(__dirname + "/files_to_send" + "/bitcoin.pdf");
 });

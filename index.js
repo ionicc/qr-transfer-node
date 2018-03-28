02bffa22804c8028f76fa44fe276291c1a96f1f8
Sagar Vakkala <sagarwakawaka@gmail.com>
2018-03-29 01:13:00 +0530

    added finding ipv4
    Added a functionality to automatically detect the ipv4 address of the device

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var qrTerminal = require('qrcode-terminal');
+var chalk = require('chalk');
+var fs = require('fs');
+var localIpV4Address = require('local-ipv4-address');
 
 var misc = require("./lib/misc.js");
 var constants = require("./lib/constants.js");
@@ -7,17 +10,36 @@ var constants = require("./lib/constants.js");
 var app = express();
 
 const port = 3000 || process.env.PORT;
+const pathInitial = __dirname + "/";
+var pathTotal = pathInitial + process.argv[2];
 
-var ipv4 = constants.IPv4;
+var ipv4 = "http://192.168.1.2:3000";
 
-app.listen(port, (req,res) => {
-  console.log("Server is live");
+localIpV4Address().then(function(ipAddress){
+    ipv4 = ipAddress;
+});
+
+
+if(process.argv.length != 3) {
+  console.log(chalk.red(":/ Something's missing in the arguments"));
+  console.log(chalk.green("It should be something like node index.js i_am_so_great.pdf"));
+  process.exit(0);
+}
+
+if(!fs.existsSync(pathTotal)) {
+  console.log(chalk.red("The file does not exist, Please check the file path again"));
+}
+else {
+
+  app.listen(port, (req,res) => {
+  //console.log("Server is live");
   misc.header();
   qrTerminal.generate(ipv4);
 
 });
 
 app.get('/', (req,res) => {
-  console.log("A GET req to the server was recieved");
-  res.sendFile(__dirname + "/files_to_send" + "/" + process.argv[2]);
+  console.log(`A download was observed from ${req.ip}`);
+  res.sendFile(pathInitial + process.argv[2]);
   });
+}
